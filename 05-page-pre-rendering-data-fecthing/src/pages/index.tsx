cbad2967cb3213e49a885d10ba3a40ed6dda3a1f
HakSooKim <haksoo@glassdomeinc.com>
2023-03-03 01:20:00 +0900

    feat: section 5 (88 - 93)

--- 05-page-pre-rendering-data-fecthing/src/pages/index.tsx
+++ 05-page-pre-rendering-data-fecthing/src/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function HomePage() {
-  return <div>HomePage</div>;
+type Product = { id: string; title: string; description: string };
+
+interface Props {
+  products: Product[];
+}
+
+export default function HomePage({ products }: Props) {
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+// this function prepares the props for the component
+// Next js will execute this function first, then execute component function
+// this function won't be part of bundle which means that never be visible on the client site
+// this function runs on server side
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
 }
